2abf2c62c01201bd344642f6ad38d09e0dd2d894
Prince Singh <Princesinghps1619@gmail.com>
2024-05-25 11:46:40 +0530

    first commit

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model.js";
+
+export const verifyJWT = asyncHandler(async(req, _, next) =>{
+    //when a parameter is declared but not used, then you can replace it with _ (underscore) e.g.->> res replaced by _
+    try {
+        const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
+    
+        if(!token){
+            throw new ApiError(401, "Unauthorized request")
+        }
+    
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+        if(!user){
+            // TODO NEXT_VIDEO
+            throw new ApiError(401, "Invalid Access Token")
+        }
+    
+        req.user = user;
+        next()
+    } catch (error) {
+        throw new ApiError(401, error?.message || "Invalid access token")
+    }
+})
